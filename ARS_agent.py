63558c0488c68b6d5f94e5a69f3cb06e5f25491d
Romain Deffayet <romain.deffayet@telecom-paristech.fr>
2018-10-17 16:31:48 +0200

    Add agent and main program

---
+++ ARS_agent.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 16:04:43 2018
+
+@author: Romain Deffayet
+"""
+
+class ARS_agent():
+    def __init__(self, ...):
+        #Initialise policy and hyperparaleters
+    
+    def train(self, ...):
+        #for loop
+        #choose_deltas
+        #evaluate finite differences
+        #choose_best_improvements
+        #average the whole thing
+        
+    def sample_deltas(self, ...):
+    
+    
+    def evaluate(self, policy):
+        
+    
+    def keep_best(self, ...):
+        
+        
+    def improve(self, ...):
+        
